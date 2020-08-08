cd879f1c81597b8873ce35a1385e184378e1f7ad
ajipratama11 <47959982+ajipratama11@users.noreply.github.com>
2020-08-08 11:03:00 +0700

    a
    a

--- application/controllers/Beranda.php
+++ application/controllers/Beranda.php
@@ -1,12 +1,11 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Pintasan extends CI_Controller
+class Beranda extends CI_Controller
 {
     function __construct()
     {
         parent::__construct();
-
         $this->load->helper('array');
         $this->load->library('form_validation');
         $this->load->helper(array('form', 'url'));
@@ -19,7 +18,7 @@ class Pintasan extends CI_Controller
     }
 
     public function index(){
-        redirect('Customer/Beranda');
+        $this->load->view('v_beranda');
     }
     public function admin(){
         redirect('Admin/Beranda');
