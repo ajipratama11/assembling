1bbefd86e78dce3def7c3d81681b5c1c00de75a5
Ardiyan16 <47960612+Ardiyan16@users.noreply.github.com>
2020-08-10 19:23:11 +0700

    baru

--- application/models/M_Rekdis.php
+++ application/models/M_Rekdis.php
@@ -16,6 +16,11 @@ class M_Rekdis extends CI_Model
         return $this->db->get($this->_table)->result();
     }
 
+    public function get_byid($no_rekdis)
+    {
+        return $this->db->get_where($this->_table, ['no_rekdis' => $no_rekdis])->row();
+    }
+
     public function add_rekdis()
     {
         $post = $this->input->post();
@@ -26,6 +31,18 @@ class M_Rekdis extends CI_Model
         $this->alamat = $post['alamat'];
 
         $this->db->insert($this->_table, $this);
+    }
+
+    public function update_rekdis($no_rekdis)
+    {
+        $post = $this->input->post();
+        $this->no_rekdis = $post['no_rekdis'];
+        $this->nama_pasien = $post['nama_pasien'];
+        $this->tgl_lahir = $post['tgl_lahir'];
+        $this->jenis_kelamin = $post['jenis_kelamin'];
+        $this->alamat = $post['alamat'];
+
+        $this->db->update($this->_table,$this, array("no_rekdis" => $no_rekdis));
     }
 
 
